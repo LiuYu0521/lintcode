77917ed4f8b0ecd9ef45886f4c85167d90f4c1e3
ly0521 <ly819933137@gmail.com>
2017-06-03 11:22:52 +0800

    子数组和变为数组累加值相减

--- 排序/139.最接近零的子数组和.cpp
+++ 排序/139.最接近零的子数组和.cpp
@@ -7,6 +7,29 @@ public:
      */
     vector<int> subarraySumClosest(vector<int> nums){
         // write your code here
-
+        vector<pair<int,int>> sum;
+        int temp = 0;
+        sum.push_back(make_pair(temp,-1));
+        for(int i = 0; i < nums.size(); i++)
+        {
+            temp = temp + nums[i];
+            sum.push_back(make_pair(temp, i));
+        }
+        sort(sum.begin(), sum.end());
+        int start,end;
+        int min_sub = INT_MAX;
+        for(int i = 1; i < sum.size(); i++)
+        {
+            if(abs(sum[i].first - sum[i - 1].first) <= min_sub)
+            {
+                min_sub = abs(sum[i].first - sum[i - 1].first);
+                start = min(sum[i].second, sum[i-1].second) + 1;
+                end = max(sum[i].second, sum[i-1].second);
+            }
+        }
+        vector<int> ret;
+        ret.push_back(start);
+        ret.push_back(end);
+        return ret;
     }
 };
