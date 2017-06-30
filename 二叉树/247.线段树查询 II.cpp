15ded6e8adb749c43accb15408ddd99bf3ec40e8
LiuYu0521 <ly819933137@gmail.com>
2017-06-30 21:12:38 +0800

    注意题目本身数据结构的意义

---
+++ 二叉树/247.线段树查询 II.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition of SegmentTreeNode:
+ * class SegmentTreeNode {
+ * public:
+ *     int start, end, count;
+ *     SegmentTreeNode *left, *right;
+ *     SegmentTreeNode(int start, int end, int count) {
+ *         this->start = start;
+ *         this->end = end;
+ *         this->count = count;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     *@param root, start, end: The root of segment tree and
+     *                         an segment / interval
+     *@return: The count number in the interval [start, end]
+     */
+    int query(SegmentTreeNode *root, int start, int end) {
+        // write your code here
+    }
+};
