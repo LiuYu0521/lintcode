dd689606bc2f7e729976bf3318b171f5681807a3
LiuYu0521 <ly819933137@gmail.com>
2017-07-04 20:33:50 +0800

    需要全局变量，可以外面设一个，然后函数里面让全局变量等于输入参量

--- 堆/612.K 个最近的点.cpp
+++ 堆/612.K 个最近的点.cpp
@@ -7,6 +7,25 @@
  *     Point(int a, int b) : x(a), y(b) {}
  * };
  */
+Point originPoint;
+int getDistance(Point &a, Point &b)
+{
+    return (a.x-b.x) * (a.x-b.x) + (a.y - b.y) * (a.y - b.y);
+}
+struct cmp
+{
+    bool operator () (Point &a, Point &b)
+    {
+        int d_a = getDistance(a, originPoint);
+        int d_b = getDistance(b, originPoint);
+        if(d_a != d_b)
+            return d_a < d_b;
+        else if(a.x != b.x)
+            return a.x < b.x;
+        else
+            return a.y < b.y;
+    }
+};
 class Solution {
 public:
     /**
@@ -17,5 +36,21 @@ public:
      */
     vector<Point> kClosest(vector<Point>& points, Point& origin, int k) {
         // Write your code here
+        priority_queue<Point, vector<Point>, cmp> q;
+        originPoint = origin;
+        for(Point p : points)
+        {
+            q.push(p);
+            if(q.size() > k)
+                q.pop();
+        }
+        vector<Point> res;
+        while(!q.empty())
+        {
+            res.push_back(q.top());
+            q.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
