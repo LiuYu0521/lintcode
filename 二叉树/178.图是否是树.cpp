720d5b2c7dc56f93e1fffa6f709c3c57072167c3
LiuYu0521 <ly819933137@gmail.com>
2017-07-06 19:34:29 +0800

    没有环并且egdes=n-1

---
+++ 二叉树/178.图是否是树.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    /**
+     * @param n an integer
+     * @param edges a list of undirected edges
+     * @return true if it's a valid tree, or false
+     */
+    bool validTree(int n, vector<vector<int>>& edges) {
+        // Write your code here
+        vector<int> root(n, -1);
+        for(int i = 0; i < edges.size(); i++)
+        {
+            int root1 = find(root, edges[i][0]);
+            int root2 = find(root, edges[i][1]);
+            if(root1 == root2)
+                return false;
+            root[root1] = root2;
+        }
+        return edges.size() == n - 1;
+    }
+    int find(vector<int> &root, int e)
+    {
+        while(root[e] != -1)
+            e = root[e];
+        return e;
+    }
+};
