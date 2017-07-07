650deac2ced7a28e82cc818d6d2d93df735aad40
LiuYu0521 <ly819933137@gmail.com>
2017-07-07 21:31:15 +0800

    vector can replace map

--- 哈希函数/647.Substring Anagrams.cpp
+++ 哈希函数/647.Substring Anagrams.cpp
@@ -7,5 +7,24 @@ public:
      */
     vector<int> findAnagrams(string& s, string& p) {
         // Write your code here
+        vector<int> res;
+        if(s.length() == 0 || p.length() == 0)
+            return res;
+        vector<int> m1(256, 0), m2(256,0);
+        for(int i = 0; i < p.size(); i++)
+        {
+            m1[s[i]]++;
+            m2[p[i]]++;
+        }
+        if(m1 == m2)
+            res.push_back(0);
+        for(int i = p.size(); i < s.size(); i++)
+        {
+            m1[s[i]]++;
+            m1[s[i - p.size()]]--;
+            if(m1 == m2)
+                res.push_back(i - p.size() + 1);
+        }
+        return res;
     }
 };
