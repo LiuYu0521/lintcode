754fcf66fbeb2beea1f51758500073ba727cf08d
LiuYu0521 <ly819933137@gmail.com>
2017-07-11 20:34:36 +0800

    update

---
+++ 递归/33.N 皇后问题.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     * Get all distinct N-Queen solutions
+     * @param n: The number of queens
+     * @return: All distinct solutions
+     * For example, A string '...Q' shows a queen on forth position
+     */
+    vector<vector<string> > solveNQueens(int n) {
+        // write your code here
+    }
+};
