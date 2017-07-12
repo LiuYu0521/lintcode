41fdb4824889e767ec44049c6c36665d19f280b1
LiuYu0521 <ly819933137@gmail.com>
2017-07-12 19:59:20 +0800

    n-queens

--- 递归/33.N 皇后问题.cpp
+++ 递归/33.N 皇后问题.cpp
@@ -8,5 +8,45 @@ public:
      */
     vector<vector<string> > solveNQueens(int n) {
         // write your code here
+        vector<vector<string>> res;
+        vector<int> pos(n, -1);
+        dfs(pos, 0, res);
+        return res;
+    }
+    void dfs(vector<int> &pos, int row, vector<vector<string>> &res)
+    {
+        int n = pos.size();
+        if(row == n)
+        {
+            vector<string> out(n, string(n, '.'));
+            for(int i = 0; i < n; i++)
+            {
+                out[i][pos[i]] = 'Q';
+            }
+            res.push_back(out);
+        }
+        else
+        {
+            for(int col = 0; col < n; col++)
+            {
+                if(isValid(row, col, pos))
+                {
+                    pos[row] = col;
+                    dfs(pos, row + 1, res);
+                    pos[row] = -1;
+                }
+            }
+        }
+    }
+    bool isValid(int row, int col, vector<int> &pos)
+    {
+        for(int i = 0; i < row; i++)
+        {
+            if(pos[i] == col || abs(row - i) == abs(col - pos[i]))
+                return false;
+        }
+        return true;
     }
 };
+// 在同一个对角线判断|row1 - row2| = |column1 - column2|
+// 循环里面有递归，要注意reset状态
