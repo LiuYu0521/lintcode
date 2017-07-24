16495167d138a6975e4f0ed263711d3dd9fe75f2
LiuYu0521 <ly819933137@gmail.com>
2017-07-24 19:42:25 +0800

    const用法

--- 数组/391.数飞机.cpp
+++ 数组/391.数飞机.cpp
@@ -13,7 +13,26 @@ public:
      * @param intervals: An interval array
      * @return: Count of airplanes are in the sky.
      */
+    priority_queue<int, vector<int>, greater<int>> q;
+    static bool cmp(const Interval &a, const Interval &b)
+    {
+        return a.start < b.start;
+    }
+    static int max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
     int countOfAirplanes(vector<Interval> &airplanes) {
         // write your code here
+        sort(airplanes.begin(), airplanes.end(), cmp);
+        int res = 0;
+        for(int i = 0; i < airplanes.size(); i++)
+        {
+            while(q.size() > 0 && q.top() <= airplanes[i].start)
+                q.pop();
+            q.push(airplanes[i].end);
+            res = max(res, q.size());
+        }
+        return res;
     }
 };
